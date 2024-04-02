702dab7115d709d4a82f26d1457f7b5b75669842
PaniniAuNutella <60790937+PaniniAuNutella@users.noreply.github.com>
2024-04-02 14:41:29 +0200

    🔧 Important modifications

--- B3_main.py
+++ B3_main.py
@@ -5,22 +5,29 @@ import numpy as np
 from B3_read import *
 
 
-# créer la suite de menu suivant:
-# Choisir un graphe parmi les 12 (entrer un chiffre de 1 à 12)
-# afficher le menu suivant pour le graphe :
-# 1. Afficher la matrice => affichera la matrice du graphe
-# 2 quitter
-
+# Création d'un menu de direction
 def menu():
     print("Choisir un graphe parmi les 12 (entrer un chiffre de 1 à 12)")
-    graph_number = int(input())
+    while True:
+        graph = int(input())
+        if 1 <= graph <= 12:
+            break
+        print("Veuillez entrer un chiffre entre 1 et 12")
     print('1. Afficher la matrice')
     print("2. quitter")
-    choice = int(input())
-    return graph_number, choice
+    while True:
+        choice = int(input())
+        if choice == 1 or choice == 2:
+            break
+        print("Veuillez entrer un chiffre entre 1 et 2")
+    return graph, choice
 
 
-# afficher la matrice
-graph_number, choice = menu()
-data = read_data(graph_number)
-print(data)
+# Execution du menu
+graph_number, menu_choice = menu()
+
+# Choix 1: Afficher la matrice
+if menu_choice == 1:
+    data = read_data(graph_number)
+    print(data)
+
